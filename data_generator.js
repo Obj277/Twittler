640153422831108b039777e6624044d0ea32f3df
zao1214 <hexa1214@gmail.com>
2017-10-13 14:23:06 +0900

    Twittlergit statusgit status

---
+++ data_generator.js
@@ -0,0 +1,86 @@
+/*
+ * NOTE: This file generates fake tweet data, and is not intended to be part of your implementation.
+ * You can safely leave this file untouched, and confine your changes to index.html.
+ */
+
+// set up data structures
+window.streams = {};
+streams.home = [];
+streams.users = {};
+streams.users.ingikim = [];
+streams.users.yannheo = [];
+streams.users.hoyoungjung = [];
+streams.users.julie = [];
+window.users = Object.keys(streams.users);
+
+// utility function for adding tweets to our data structures
+var addTweet = function(newTweet){
+  var username = newTweet.user;
+  if(streams.users[username]){
+      var user1stObj = streams.users[username][0];
+      if(user1stObj !== undefined){
+          newTweet.userImgPath = user1stObj.userImgPath;
+      }
+  }else{
+      streams.users[username] = [];
+  }
+
+  streams.users[username].push(newTweet);
+  streams.home.push(newTweet);
+};
+
+// utility function
+var randomElement = function(array){
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
+
+// random user img generator
+var imgs = ['img/boy.png','img/female1.png','img/girl.png','img/male3.png','img/malecostume.png','img/matureman1.png','img/matureman2.png','img/maturewoman.png']
+
+// random tweet generator
+var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
+var verbs = ['drank', 'drunk', 'deployed', 'got', 'developed', 'built', 'invented', 'experienced', 'fought off', 'hardened', 'enjoyed', 'developed', 'consumed', 'debunked', 'drugged', 'doped', 'made', 'wrote', 'saw'];
+var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
+var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
+var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
+
+var randomMessage = function(){
+  return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns)].join(' ');
+};
+
+// generate random tweets on a random schedule
+var generateRandomTweet = function(){
+  var tweet = {};
+  tweet.user = randomElement(users);
+  tweet.userImgPath = randomElement(imgs);
+  tweet.message = randomMessage();
+  tweet.created_at = new Date();
+  tweet.tag = randomElement(tags);
+  addTweet(tweet);
+};
+
+for(var i = 0; i < 10; i++){
+  generateRandomTweet();
+}
+
+var scheduleNextTweet = function(){
+  generateRandomTweet();
+  setTimeout(scheduleNextTweet, Math.random() * 1500);
+};
+scheduleNextTweet();
+
+// utility function for letting students add "write a tweet" functionality
+// (note: not used by the rest of this file.)
+var writeTweet = function(message){
+  if(!visitor){
+    throw new Error('set the global visitor property!');
+  }
+  var tweet = {};
+  tweet.user = visitor;
+  tweet.message = message;
+  tweet.userImgPath = visitorImg;
+  tweet.created_at = new Date();
+  tweet.tag = visitorTags;
+  addTweet(tweet);
+};
